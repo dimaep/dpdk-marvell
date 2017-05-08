f40e98bb014bef051fc89e9cc9b0d8604ba2a04a
Tomasz Duszynski <tdu@semihalf.com>
2017-05-08 08:38:18 +0200

    examples: l2fwd: revert "examples: l2fwd: bring link up after rte_eth_dev_start()"
    This reverts commit 0ffee77a237225c1c0f6cd91857a6bcc03bc4539 as its
    no longer necessary. Link is brought up automatically after device is
    started.

--- examples/l2fwd/main.c
+++ examples/l2fwd/main.c
@@ -710,18 +710,6 @@ main(int argc, char **argv)
 			rte_exit(EXIT_FAILURE, "rte_eth_dev_start:err=%d, port=%u\n",
 				  ret, (unsigned) portid);
 
-		/*
-		 * TODO: link is not brought up during rte_eth_dev_start()
-		 * as doing so hangs the board. Bring it up here as
-		 * a temporary workaround.
-		 */
-		ret = rte_eth_dev_set_link_up(portid);
-		if (ret < 0)
-			rte_exit(EXIT_FAILURE, "rte_eth_dev_set_link_up:err=%d, port=%u\n",
-				ret, portid);
-
-		printf("done: \n");
-
 		rte_eth_promiscuous_enable(portid);
 
 		printf("Port %u, MAC address: %02X:%02X:%02X:%02X:%02X:%02X\n\n",
